1953e1a4bdd61df563253cdb316568cb18233fa8
= <postagestamp@windowslive.com>
2024-10-04 10:50:31 +1300

    Add pulsative darkening, optimise palinopsia

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const snowCnv = document.getElementById('snow');
 const palinopsiaCnv = document.getElementById('palinopsia');
 const photopsiaCnv = document.getElementById('photopsia');
 const BFEPCnv = document.getElementById('BFEP');
-const delay = 100;
+const pulsativeCnv = document.getElementById('pulsative');
+const delay = 25;
 
 function toHex(r, g, b, a) {
 	return ((a<<24>>>0)+(b<<16>>>0)+(g<<8>>>0)+(r<<0>>>0));
 }
 
 function fromHex(hex) {
-	let a = (hex & 0xFF000000)>>>24;
-  let b = (hex & 0x00FF0000)>>>16;
-  let g = (hex & 0x0000FF00)>>>8;
-  let r = (hex & 0x000000FF)>>>0;
-  return [r, g, b, a];
+    let a = (hex & 0xFF000000) >>> 24;
+    let b = (hex & 0x00FF0000)>>>16;
+    let g = (hex & 0x0000FF00)>>>8;
+    let r = (hex & 0x000000FF)>>>0;
+    return [r, g, b, a];
 }
 
 class Renderable {
@@ -44,8 +45,8 @@ class Renderable {
 
 function snowRenderFunc(context, tick) {
 	if (tick == 0) {
-    const idata = context.createImageData(context.canvas.width, context.canvas.height);
-    const buffer32 = new Uint32Array(idata.data.buffer);  // get 32-bit view
+        const idata = context.createImageData(context.canvas.width, context.canvas.height);
+        const buffer32 = new Uint32Array(idata.data.buffer);  // get 32-bit view
     function noise() {
       var len = buffer32.length - 1;
       while(len--) buffer32[len] = Math.random() < 0.8 ? 0 : toHex(255, 255, 255, 20);
@@ -59,43 +60,65 @@ function snowRenderFunc(context, tick) {
 
 function palinopsiaRenderFunc(context, tick) {
 	if (tick == 0) {
-    var bgCnv = document.createElement('canvas');
-    var bgCtx = bgCnv.getContext('2d');
-    var img = document.getElementById('bg');
-    bgCnv.width = img.width;
-    bgCnv.height = img.height;
-    bgCtx.drawImage(img, 0, 0 );
-    bgCnv.style.marginTop = '500px';
-    var bgData = bgCtx.getImageData(0, 0, img.width, img.height);
-    var bgBuffer = new Uint32Array(bgData.data.buffer);
-    let baseYShift = 25;
-    let msPerYCycle = 2000;
-    let ticksPerYCycle = msPerYCycle / delay;
-    const idata = context.createImageData(context.canvas.width, context.canvas.height);
-    const getCoords = n => [n % context.canvas.width, Math.floor(n / context.canvas.width)];
-    const [centX, centY] = [Math.floor(context.canvas.width/2), Math.floor(context.canvas.height/2)];
-    const buffer32 = new Uint32Array(idata.data.buffer);
-    function noise(ctx, tick) {
-      var len = buffer32.length - 1;
-      let yShiftPct = (tick % (2*ticksPerYCycle)) / ticksPerYCycle;
-      if (yShiftPct > 1) yShiftPct = 1 - (yShiftPct - 1);
-      while(len--) {
-        let [x, y] = getCoords(len);
-        let yDist = Math.abs(y - centY) / (context.canvas.height/2);
-        let xDist = Math.abs(x - centX) / (context.canvas.width/2);
-        let dist = 255-Math.floor(((xDist**2+yDist**2)**.5)/(2**.5)*255);
-        let yShift = Math.floor(baseYShift * yShiftPct);
-        let newLen = y < (context.canvas.height - yShift) ? (len + yShift * context.canvas.width) : y;
-        let [r, g, b, a] = fromHex(bgBuffer[newLen]);
-        let newColour = toHex(255-r, 255-g, 255-b, Math.floor(dist/4));
-        buffer32[len] = newColour;
-      }
-      context.putImageData(idata, 0, 0);
+        var bgCnv = document.createElement('canvas');
+        var bgCtx = bgCnv.getContext('2d');
+        var img = document.getElementById('bg');
+        bgCnv.width = img.width;
+        bgCnv.height = img.height;
+        bgCtx.drawImage(img, 0, 0 );
+        bgCnv.style.marginTop = '500px';
+        var bgData = bgCtx.getImageData(0, 0, img.width, img.height);
+        var bgBuffer = new Uint32Array(bgData.data.buffer);
+        let baseYShift = 25;
+        let msPerYCycle = 2000;
+        let ticksPerYCycle = msPerYCycle / delay;
+        const idata = context.createImageData(context.canvas.width, context.canvas.height);
+        const getCoords = n => [n % context.canvas.width, Math.floor(n / context.canvas.width)];
+        const [centX, centY] = [Math.floor(context.canvas.width/2), Math.floor(context.canvas.height/2)];
+        const buffer32 = new Uint32Array(idata.data.buffer);
+
+        function getYShift(tick) {
+            let yShiftPct = (tick % (2 * ticksPerYCycle)) / ticksPerYCycle;
+            if (yShiftPct > 1) yShiftPct = 1 - (yShiftPct - 1);
+            const yShift = Math.floor(baseYShift * yShiftPct);
+            return yShift;
+        }
+
+        function getRender(tick) {
+            let len = buffer32.length - 1;
+            const yShift = getYShift(tick);
+            while(len--) {
+                const [x, y] = getCoords(len);
+                const yDist = Math.abs(y - centY) / (context.canvas.height/2);
+                const xDist = Math.abs(x - centX) / (context.canvas.width/2);
+                const dist = 255-Math.floor(((xDist**2+yDist**2)**.5)/(2**.5)*255);
+                const newLen = y < (context.canvas.height - yShift) ? (len + yShift * context.canvas.width) : y;
+                const [r, g, b, a] = fromHex(bgBuffer[newLen]);
+                if (r + g + b < (200 * 3) || 1) {
+                    const newColour = toHex(255 - r, 255 - g, 255 - b, Math.floor(dist / 4));
+                    buffer32[len] = newColour;
+                }
+            }
+            return new ImageData(new Uint8ClampedArray(idata.data), idata.width, idata.height);
+        }
+        console.log('Rendering palinopsia frames...');
+        console.log(ticksPerYCycle);
+       
+        this.frames = new Array(ticksPerYCycle * 2);
+        function render(tick) {
+            const frameOffset = tick % (ticksPerYCycle * 2);
+            if (!this.frames[frameOffset]) this.frames[frameOffset] = getRender(tick);
+            context.putImageData(this.frames[frameOffset], 0, 0);
+
+        }
+        this.render = render;
+        this.getRender = getRender;
+        console.log('Finished');
+
+        
+    } else {
+        this.render(tick);
     }
-    this.noise = noise;
-  } else {
-  	this.noise(context, tick);
-  }
 }
 
 function photopsiaRenderFunc(context, tick) {
@@ -154,44 +177,44 @@ function photopsiaRenderFunc(context, tick) {
 
 function BFEPRenderFunc(context, tick) {
 	if (tick == 0) {
-  	this.entities = [];
-    this.meanMsPerSpawn = 100;
-    this.msLifeCycle = 2000;
-    this.meanTicksPerSpawn = this.meanMsPerSpawn / this.delay;
-    this.tickLifeCycle = this.msLifeCycle / this.delay;
-    const radius = 8;
-    this.speed = 10;
-    this.radius = radius;
-    this.nextSpawn = this.meanTicksPerSpawn;
-    function spawn(tick, ents) {
-    	let x = Math.floor(Math.random() * context.canvas.width);
-      let y = Math.floor(Math.random() * context.canvas.height);
-      let newEnt = {
-      	x: x,
-        y: y,
-        startTick: tick,
-        endTick: tick + this.tickLifeCycle,
-        radius: this.radius,
-      }
-      let setGradient = () => {
-      	let gradient = context.createRadialGradient(
-        	newEnt.x, newEnt.y, 1, 
-          newEnt.x, newEnt.y, radius
-        );
-        gradient.addColorStop(0, 'rgba(255,255,230,.05)');
-        gradient.addColorStop(.4, 'rgba(125,125,125,.1)');
-        gradient.addColorStop(.5, 'rgba(125,125,125,.3)');
-        gradient.addColorStop(.6, 'rgba(125,125,125,.1)');
-        gradient.addColorStop(1, 'rgba(94,30,255,.05)');
-        newEnt.fill = gradient;
-      }
-      newEnt.setGradient = setGradient;
-      newEnt.setGradient();
-    	ents.push(newEnt);
-      this.nextSpawn = tick + Math.floor((Math.random() * 2) * this.meanTicksPerSpawn) + 1;
-    }
-    spawn(tick, this.entities);
-    this.spawn = spawn;
+        this.entities = [];
+        this.meanMsPerSpawn = 100;
+        this.msLifeCycle = 2000;
+        this.meanTicksPerSpawn = this.meanMsPerSpawn / this.delay;
+        this.tickLifeCycle = this.msLifeCycle / this.delay;
+        const radius = 8;
+        this.speed = 10;
+        this.radius = radius;
+        this.nextSpawn = this.meanTicksPerSpawn;
+        function spawn(tick, ents) {
+            let x = Math.floor(Math.random() * context.canvas.width);
+            let y = Math.floor(Math.random() * context.canvas.height);
+            let newEnt = {
+                x: x,
+                y: y,
+                startTick: tick,
+                endTick: tick + this.tickLifeCycle,
+                radius: this.radius,
+            };
+            let setGradient = () => {
+                let gradient = context.createRadialGradient(
+                    newEnt.x, newEnt.y, 1, 
+                    newEnt.x, newEnt.y, radius
+                );
+                gradient.addColorStop(0, 'rgba(255,255,230,.05)');
+                gradient.addColorStop(.4, 'rgba(125,125,125,.1)');
+                gradient.addColorStop(.5, 'rgba(125,125,125,.3)');
+                gradient.addColorStop(.6, 'rgba(125,125,125,.1)');
+                gradient.addColorStop(1, 'rgba(94,30,255,.05)');
+                newEnt.fill = gradient;
+            }
+            newEnt.setGradient = setGradient;
+            newEnt.setGradient();
+            ents.push(newEnt);
+            this.nextSpawn = tick + Math.floor((Math.random() * 2) * this.meanTicksPerSpawn) + 1;
+        }
+        spawn(tick, this.entities);
+        this.spawn = spawn;
   } else {
   	if (tick == this.nextSpawn) this.spawn(tick, this.entities);
   	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
@@ -210,6 +233,27 @@ function BFEPRenderFunc(context, tick) {
     }
     this.entities = newEnts;
   }
+}
+
+function pulsativeRenderFunc(context, tick) {
+    if (tick == 0) {
+        this.pulseIntervalMs = 5000;
+        this.pulseIntervalTicks = this.pulseIntervalMs / delay;
+        this.pulseDurationMs = 500;
+        this.pulseDurationTicks = this.pulseDurationMs / delay;
+        this.pulseDepth = 0.5;
+    } else {
+        let currentOffset = tick % this.pulseIntervalTicks;
+        let depth;
+        if (currentOffset > this.pulseDurationTicks) depth = 0;
+        else {
+            let depthOffset = Math.abs((this.pulseDurationTicks / 2) - currentOffset) / (this.pulseDurationTicks / 2);
+            depth = this.pulseDepth - (this.pulseDepth * depthOffset);
+        }
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = 'rgba(0,0,0,' + depth + ')';
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }
 }
 
 
@@ -245,3 +289,5 @@ let photopsia = new Renderable(photopsiaRenderFunc, photopsiaCnv, delay);
 photopsia.start();
 let BFEP = new Renderable(BFEPRenderFunc, BFEPCnv, delay);
 BFEP.start();
+let pulsative = new Renderable(pulsativeRenderFunc, pulsativeCnv, delay);
+pulsative.start();
